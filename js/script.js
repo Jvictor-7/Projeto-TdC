ac9b424676020dfb3d7e2729844785d8073f462e
Jvictor-7 <jvcl@ic.ufal.br>
2022-10-13 12:45:36 -0300

    Descrição quando passa o mouse em cima dos estados adicionadas!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+
 function btnClick(e){
     const btn = document.querySelectorAll("button");
-
+    
     btn.forEach(element => {
         if(element.classList.contains("actived") && element.id != e ){
             element.classList.remove("actived");
@@ -10,8 +11,9 @@ function btnClick(e){
 }
 
 function btnConfirmClick(){
-
+    
     let entrada = document.getElementById('entrada').value;
+
     var error = document.getElementById("error");
 
     if(entrada == ''){
@@ -86,7 +88,6 @@ function TransitionState(state){
 
                 stateDiv.classList.remove('activedState');
                 stateDiv.classList.add('state10');
-
                 document.getElementById(state).classList.remove('actived');
 
                 btnConfirm.removeAttribute('disabled')
@@ -97,4 +98,45 @@ function TransitionState(state){
         
        
     }, 1000);
+
+}
+
+function descricao(val) {
+    var descricaoElemento = document.getElementById('descricao');
+    console.log(val)
+    switch (val) {
+        case 0:
+            descricaoElemento.innerHTML = 'Estado q0: Esperando ação do cliente...'
+            break
+        case 1:
+            descricaoElemento.innerHTML = 'Estado q1: Resgatando o alimento (Biscoito Negresco)'
+            break
+        case 2:
+            descricaoElemento.innerHTML = 'Estado q2: Resgatando o alimento (Biscoito Passatempo)'
+            break
+        case 3:
+            descricaoElemento.innerHTML = 'Estado q3: Resgatando o alimento (Biscoito Bono)'
+            break
+        case 4:
+            descricaoElemento.innerHTML = 'Estado q4: Resgatando o alimento (Salgadinho Ruffles)'
+            break
+        case 5:
+            descricaoElemento.innerHTML = 'Estado q5: Resgatando o alimento (Salgadinho Cheetos)'
+            break
+        case 6:
+            descricaoElemento.innerHTML = 'Estado q6: Resgatando o alimento (Salgadinho Doritos)'
+            break
+        case 7:
+            descricaoElemento.innerHTML = 'Estado q7: Resgatando o alimento (Refrigerante Coca-cola)'
+            break
+        case 8:
+            descricaoElemento.innerHTML = 'Estado q8: Resgatando o alimento (Guaraná Antártica)'
+            break
+        case 9:
+            descricaoElemento.innerHTML = 'Estado q3: Resgatando o alimento (Energético Redbull)'
+            break
+        case 10:
+            descricaoElemento.innerHTML = 'Estado q10: Entregando o alimento...'
+            break       
+    }
 }
